f778ac365a2eaa382cd70e57a4f65ee3799bd0d2
Maximiliano Zavala <m.zavala22@tamu.edu>
2024-06-16 15:50:48 -0700

    Routes working, custom DNS working, width specific video

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../css/home.css';
 
-const desktopVid = `${process.env.PUBLIC_URL}/assets/tsotuyo-desktop.mp4`;
-const mobileVid = `${process.env.PUBLIC_URL}/assets/tsotuyo-mobile.mp4`;
+const desktopVid = `${process.env.PUBLIC_URL}/assets/tsotuyo-desktop-noaudio-1080.mp4`;
+const desktopPoster = `${process.env.PUBLIC_URL}/assets/tsotuyo-desktop-tn.png`;
+const mobileVid = `${process.env.PUBLIC_URL}/assets/tsotuyo-mobile-noaudio-1080.mp4`;
+const mobilePoster = `${process.env.PUBLIC_URL}/assets/tsotuyo-mobile-tn.png`;
 
 export const Home = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 768);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className="home-container">
-      <video className="video-background" autoPlay loop muted>
-        <source src={mobileVid} type="video/mp4" media="(max-width: 768px)" />
-        <source src={desktopVid} type="video/mp4" media="(min-width: 769px)" />
+      <video
+        className="video-background"
+        muted
+        playsInline
+        autoPlay
+        loop
+        poster={isMobile ? mobilePoster : desktopPoster}
+      >
+        <source src={isMobile ? mobileVid : desktopVid} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
