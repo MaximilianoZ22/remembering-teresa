11b5213bb347bebd3a2a3d3a5e139998b2f69e3d
Maximiliano Zavala <m.zavala22@tamu.edu>
2024-06-16 04:12:10 -0700

    updating homepage

--- src/pages/memories.js
+++ src/pages/memories.js
@@ -1,7 +1,23 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ServiceInfo from '../components/serviceInfo';
+import '../css/service.css';
+
 export const Memories = () => {
     return (
-      <div>
-        <h1>Memories</h1>
+      <div className='body'>
+      <Container className="service-container rubik-regular">
+        <Row >
+          <Col>
+            <h1 style={{ fontSize: '70px', paddingLeft:"5%" }}>Memories</h1>
+          </Col>
+        </Row>
+        <Row className="mb-4">
+          images
+        </Row>
+      </Container>
       </div>
     );
   }
