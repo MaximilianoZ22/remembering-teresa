99b153906a3499a3e88cf814ba96196b4111e32a
Maximiliano Zavala <m.zavala22@tamu.edu>
2024-06-16 23:53:19 -0700

    Updating image gallery

--- src/components/memories/imageGallery.js
+++ src/components/memories/imageGallery.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Gallery } from "react-grid-gallery";
 
-const ImageGallery = ({ imageList }) => (
-  <div className="image-gallery">
-    {imageList.map((url, index) => (
-      <img key={index} src={url} alt="Uploaded Memory" /> // Display each image
-    ))}
-  </div>
-);
+const ImageGallery = ({ imageList }) => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const loadImage = (url) => {
+      return new Promise((resolve) => {
+        const img = new Image();
+        img.onload = () => {
+          resolve({
+            src: url,
+            width: img.width,
+            height: img.height,
+            alt: "Uploaded Memory",
+          });
+        };
+        img.src = url;
+      });
+    };
+
+    const loadImages = async () => {
+      const imagePromises = imageList.map((url) => loadImage(url));
+      const imagesData = await Promise.all(imagePromises);
+      setImages(imagesData);
+    };
+
+    loadImages();
+  }, [imageList]);
+
+  return (
+    <div className="image-gallery">
+      <Gallery images={images} />
+    </div>
+  );
+};
 
 export default ImageGallery;
